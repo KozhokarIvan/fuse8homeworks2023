66d95ea3b1c34eb4d3ab1e16f01fac3ad7e4d2b6
KozhokarIvan <kozhokar.iv@yandex.ru>
2023-07-24 19:22:30 +0300

    made LazyTests pass

--- Homework2/Domain/Lazy.cs
+++ Homework2/Domain/Lazy.cs
@@ -5,7 +5,25 @@
 /// </summary>
 public class Lazy<TValue>
 {
-	// TODO Реализовать ленивое получение значение при первом обращении к Value
+    // TODO Реализовать ленивое получение значение при первом обращении к Value
 
-	public TValue? Value { get; }
+    private bool _isCalculated = false;
+    private TValue? _value;
+    public TValue? Value
+    {
+        get
+        {
+            if (!_isCalculated)
+            {
+                _isCalculated = true;
+                _value = _func.Invoke();
+            }
+            return _value;
+        }
+    }
+    public readonly Func<TValue?> _func;
+    public Lazy(Func<TValue?> value)
+    {
+        _func = value;
+    }
 }
