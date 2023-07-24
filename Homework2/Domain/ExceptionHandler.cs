22ad9019dafb5a0780faeee66cfe7487030dd15c
KozhokarIvan <kozhokar.iv@yandex.ru>
2023-07-24 19:22:21 +0300

    made ExceptionHandlerTests pass

--- Homework2/Domain/ExceptionHandler.cs
+++ Homework2/Domain/ExceptionHandler.cs
@@ -4,41 +4,64 @@ namespace Fuse8_ByteMinds.SummerSchool.Domain;
 
 public static class ExceptionHandler
 {
-	/// <summary>
-	/// Обрабатывает исключение, которое может возникнуть при выполнении <paramref name="action"/>
-	/// </summary>
-	/// <param name="action">Действие, которое может породить исключение</param>
-	/// <returns>Сообщение об ошибке</returns>
-	public static string? Handle(Action action)
-	{
-		// TODO Реализовать обработку исключений
-		action();
-		return "Ok";
-	}
+    /// <summary>
+    /// Обрабатывает исключение, которое может возникнуть при выполнении <paramref name="action"/>
+    /// </summary>
+    /// <param name="action">Действие, которое может породить исключение</param>
+    /// <returns>Сообщение об ошибке</returns>
+    public static string? Handle(Action action)
+    {
+        try
+        {
+            action();
+        }
+        catch (NotValidKopekCountException)
+        {
+            return "Количество копеек должно быть больше 0 и меньше 99";
+        }
+        catch (NegativeRubleCountException)
+        {
+            return "Число рублей не может быть отрицательным";
+        }
+        catch (MoneyException ex)
+        {
+            return ex.Message;
+        }
+        catch (HttpRequestException ex)
+        {
+            return ex.StatusCode == HttpStatusCode.NotFound ? "Ресурс не райден" : ex.StatusCode.ToString();
+        }
+        catch (Exception)
+        {
+            return "Произошла непредвиденная ошибка";
+
+        }
+        return null;
+    }
 }
 
 public class MoneyException : Exception
 {
-	public MoneyException()
-	{
-	}
+    public MoneyException()
+    {
+    }
 
-	public MoneyException(string? message)
-		: base(message)
-	{
-	}
+    public MoneyException(string? message)
+        : base(message)
+    {
+    }
 }
 
 public class NotValidKopekCountException : MoneyException
 {
-	public NotValidKopekCountException()
-	{
-	}
+    public NotValidKopekCountException()
+    {
+    }
 }
 
 public class NegativeRubleCountException : MoneyException
 {
-	public NegativeRubleCountException()
-	{
-	}
+    public NegativeRubleCountException()
+    {
+    }
 }
