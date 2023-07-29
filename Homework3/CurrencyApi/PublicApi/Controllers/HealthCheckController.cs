6727f53c4f51d0cc3b7970e895db45f1908418c5
KozhokarIvan <kozhokar.iv@yandex.ru>
2023-07-29 05:16:30 +0300

    completed the assignment

--- Homework3/CurrencyApi/PublicApi/Controllers/HealthCheckController.cs
+++ Homework3/CurrencyApi/PublicApi/Controllers/HealthCheckController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fuse8_ByteMinds.SummerSchool.PublicApi.Services;
+using Microsoft.AspNetCore.Mvc;
 using static Fuse8_ByteMinds.SummerSchool.PublicApi.Controllers.HealthCheckResult;
 
 namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Controllers;
@@ -9,18 +10,34 @@ namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Controllers;
 [Route("healthcheck")]
 public class HealthCheckController : ControllerBase
 {
-	/// <summary>
-	/// Проверить что API работает
-	/// </summary>
-	/// <param name="checkExternalApi">Необходимо проверить работоспособность внешнего API. Если FALSE или NULL - проверяется работоспособность только текущего API</param>
-	/// <response code="200">
-	/// Возвращает если удалось получить доступ к API
-	/// </response>
-	/// <response code="400">
-	/// Возвращает если удалось не удалось получить доступ к API
-	/// </response>
-	[HttpGet]
-	public HealthCheckResult Check(bool? checkExternalApi) => new() { Status = CheckStatus.Ok, CheckedOn = DateTimeOffset.Now };
+    private readonly CurrencyService _currencyService;
+    public HealthCheckController(CurrencyService currencyService)
+    {
+        _currencyService = currencyService;
+    }
+
+    /// <summary>
+    /// Проверить что API работает
+    /// </summary>
+    /// <param name="checkExternalApi">Необходимо проверить работоспособность внешнего API. Если FALSE или NULL - проверяется работоспособность только текущего API</param>
+    /// <response code="200">
+    /// Возвращает если удалось получить доступ к API
+    /// </response>
+    /// <response code="400">
+    /// Возвращает если удалось не удалось получить доступ к API
+    /// </response>
+    [HttpGet]
+    public async Task<HealthCheckResult> Check(bool? checkExternalApi)
+    {
+        var checkedOn = DateTimeOffset.Now;
+        CheckStatus status;
+        if (checkExternalApi is not null && checkExternalApi == true)
+            status = await _currencyService.CheckHealth() ? CheckStatus.Ok : CheckStatus.Failed;
+        else
+            status = CheckStatus.Ok;
+        var response = new HealthCheckResult { CheckedOn = checkedOn, Status = status };
+        return response;
+    }
 }
 
 /// <summary>
@@ -28,29 +45,29 @@ public class HealthCheckController : ControllerBase
 /// </summary>
 public record HealthCheckResult
 {
-	/// <summary>
-	/// Дата проверки
-	/// </summary>
-	public DateTimeOffset CheckedOn { get; init; }
+    /// <summary>
+    /// Дата проверки
+    /// </summary>
+    public DateTimeOffset CheckedOn { get; init; }
 
-	/// <summary>
-	/// Статус работоспособности API
-	/// </summary>
-	public CheckStatus Status { get; init; }
+    /// <summary>
+    /// Статус работоспособности API
+    /// </summary>
+    public CheckStatus Status { get; init; }
 
-	/// <summary>
-	/// Статус API
-	/// </summary>
-	public enum CheckStatus
-	{
-		/// <summary>
-		/// API работает
-		/// </summary>
-		Ok = 1,
+    /// <summary>
+    /// Статус API
+    /// </summary>
+    public enum CheckStatus
+    {
+        /// <summary>
+        /// API работает
+        /// </summary>
+        Ok = 1,
 
-		/// <summary>
-		/// Ошибка в работе API
-		/// </summary>
-		Failed = 2,
-	}
+        /// <summary>
+        /// Ошибка в работе API
+        /// </summary>
+        Failed = 2,
+    }
 }
---
+++ Homework3/CurrencyApi/PublicApi/Services/CurrencyService.cs
@@ -0,0 +1,92 @@
+﻿using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using Fuse8_ByteMinds.SummerSchool.PublicApi.Exceptions;
+using Fuse8_ByteMinds.SummerSchool.PublicApi.Options;
+using Microsoft.Extensions.Options;
+
+namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Services
+{
+    public class CurrencyService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IOptionsSnapshot<CurrencyApiSettings> _options;
+        public CurrencyService(IHttpClientFactory httpClientFactory, IOptionsSnapshot<CurrencyApiSettings> options)
+        {
+            _httpClientFactory = httpClientFactory;
+            _options = options;
+        }
+        public async Task<bool> CheckHealth()
+        {
+            var httpClient = _httpClientFactory.CreateClient(CurrencyApiSettings.CurrencyApiName);
+            var response = await httpClient.GetAsync("status");
+            return response.StatusCode == HttpStatusCode.OK;
+        }
+        public async Task<(string code, decimal value)> GetDefaultCurrency()
+        {
+            var httpClient = _httpClientFactory.CreateClient(CurrencyApiSettings.CurrencyApiName);
+            var response = await httpClient.GetAsync($"latest?currencies={_options.Value.Currency}&base_currency={_options.Value.BaseCurrency}");
+            var responseString = await response.Content.ReadAsStringAsync();
+            var currencyInfo = GetJsonElementByPath(responseString, "data", _options.Value.Currency);
+            string code = currencyInfo.GetProperty("code").ToString();
+            decimal value = decimal.Parse(currencyInfo.GetProperty("value").ToString(), CultureInfo.InvariantCulture);
+            value = decimal.Round(value, _options.Value.DecimalPlaces);
+            return (code, value);
+        }
+        public async Task<(string code, decimal value)> GetCurrencyByCode(string currencyCode)
+        {
+            var httpClient = _httpClientFactory.CreateClient(CurrencyApiSettings.CurrencyApiName);
+            var response = await httpClient
+               .GetAsync($"latest?currencies={currencyCode}&base_currency={_options.Value.BaseCurrency}");
+            bool currencyDoesNotExist = response.StatusCode == HttpStatusCode.UnprocessableEntity;
+            if (currencyDoesNotExist)
+                throw new CurrencyNotFoundException(currencyCode);
+            var responseString = await response.Content.ReadAsStringAsync();
+            var jsonRootElement = JsonDocument.Parse(responseString).RootElement;
+            var currencyInfo = jsonRootElement
+                    .GetProperty("data")
+                    .GetProperty(currencyCode);
+            string code = currencyInfo.GetProperty("code").ToString();
+            decimal value = decimal.Parse(currencyInfo.GetProperty("value").ToString(), CultureInfo.InvariantCulture);
+            value = decimal.Round(value, _options.Value.DecimalPlaces);
+            return (code, value);
+        }
+        public async Task<decimal> GetCurrencyByCodeAndDate(string currencyCode, DateOnly date)
+        {
+            var httpClient = _httpClientFactory.CreateClient(CurrencyApiSettings.CurrencyApiName);
+            var response = await httpClient
+                .GetAsync($"historical?currencies={currencyCode}&date={date.ToString("yyyy-MM-dd")}&base_currency={_options.Value.BaseCurrency}");
+            bool currencyDoesNotExist = response.StatusCode == HttpStatusCode.UnprocessableEntity;
+            if (currencyDoesNotExist)
+                throw new CurrencyNotFoundException(currencyCode);
+            var responseString = await response.Content.ReadAsStringAsync();
+            var jsonRootElement = JsonDocument.Parse(responseString).RootElement;
+            decimal value = decimal
+                .Parse(jsonRootElement
+                    .GetProperty("data")
+                    .GetProperty(currencyCode)
+                    .GetProperty("value")
+                    .ToString(), CultureInfo.InvariantCulture);
+            value = decimal.Round(value, _options.Value.DecimalPlaces);
+            return value;
+        }
+        public async Task<(int requestCount, int requestLimit)> GetRequestQuotas()
+        {
+            var httpClient = _httpClientFactory.CreateClient(CurrencyApiSettings.CurrencyApiName);
+            var response = await httpClient.GetAsync("status");
+            var responseString = await response.Content.ReadAsStringAsync();
+            var requestsInfo = GetJsonElementByPath(responseString, "quotas", "month");
+            var requestsCount = int.Parse(requestsInfo.GetProperty("used").ToString());
+            var requestsLimit = int.Parse(requestsInfo.GetProperty("total").ToString());
+            return (requestsCount, requestsLimit);
+        }
+        private JsonElement GetJsonElementByPath(string jsonString, params string[] jsonPath)
+        {
+            var jsonRootElement = JsonDocument.Parse(jsonString).RootElement;
+            JsonElement jsonElement = jsonRootElement.GetProperty(jsonPath[0]);
+            for (int i = 1; i < jsonPath.Length; i++)
+                jsonElement = jsonElement.GetProperty(jsonPath[i]);
+            return jsonElement;
+        }
+    }
+}
