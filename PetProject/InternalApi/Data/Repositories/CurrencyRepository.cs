6065c4a8bc65445fd6695cbdc6478398c735ab4a
KozhokarIvan <kozhokar.iv@yandex.ru>
2023-09-02 17:33:59 +0300

    removed redundant usings

--- PetProject/InternalApi/Data/Repositories/CurrencyRepository.cs
+++ PetProject/InternalApi/Data/Repositories/CurrencyRepository.cs
@@ -1,5 +1,4 @@
-﻿using Fuse8_ByteMinds.SummerSchool.InternalApi.Data;
-using Fuse8_ByteMinds.SummerSchool.InternalApi.Data.Entities;
+﻿using Fuse8_ByteMinds.SummerSchool.InternalApi.Data.Entities;
 using Fuse8_ByteMinds.SummerSchool.InternalApi.Interfaces.Repositories;
 using Fuse8_ByteMinds.SummerSchool.InternalApi.Options;
 using Microsoft.EntityFrameworkCore;
--- PetProject/PublicApi/Data/Repositories/FavoriteExchangesRepository.cs
+++ PetProject/PublicApi/Data/Repositories/FavoriteExchangesRepository.cs
@@ -1,5 +1,4 @@
-﻿using Fuse8_ByteMinds.SummerSchool.PublicApi.Data;
-using Fuse8_ByteMinds.SummerSchool.PublicApi.Data.Entities;
+﻿using Fuse8_ByteMinds.SummerSchool.PublicApi.Data.Entities;
 using Fuse8_ByteMinds.SummerSchool.PublicApi.Interfaces.Repositories;
 using Microsoft.EntityFrameworkCore;
 
